478679829d16bb85fcbe1b2d4a672cd5e788870e
lordbryden <frutsendi@gmail.com>
2024-07-24 11:25:42 +0100

    home page

--- src/app/drawing/drawing.page.ts
+++ src/app/drawing/drawing.page.ts
@@ -91,49 +91,40 @@ export class DrawingPage  {
 
 
 
-   segments1 = [
+  segments1 = [
     // External wall
     { start: 'A1', end: 'A2', name: 'External wall 1', type: 'wall', length: 5000, angle: 0 },
-    { start: 'A2', end: 'A3', name: 'External wall 2', type: 'wall', length: 5000, angle: 90 },
+    { start: 'A2', end: 'A3', name: 'External wall 2', type: 'wall', length: 4000, angle: 90 },
     { start: 'A3', end: 'A4', name: 'External wall 3', type: 'wall', length: 5000, angle: 180 },
-    { start: 'A4', end: 'A1', name: 'External wall 4', type: 'wall', length: 5000, angle: 270 },
+    { start: 'A4', end: 'A1', name: 'External wall 4', type: 'wall', length: 4000, angle: 270 },
 
     // Inner walls Kitchen
-    { start: 'A3', end: 'A5', name: 'Inner wall 1', type: 'wall', length: 2000, angle: 270 },
-    { start: 'A5', end: 'A6', name: 'Inner wall 2', type: 'wall', length: 2500, angle: 180 },
-    { start: 'A6', end: 'A7', name: 'Inner wall 3', type: 'wall', length: 2000, angle: 90 },
+    { start: 'A3', end: 'A5', name: 'Inner wall 1', type: 'wall', length: 1500, angle: 270 },
+    { start: 'A5', end: 'A6', name: 'Inner wall 2', type: 'wall', length: 1200, angle: 180 },
+    { start: 'A6', end: 'A7', name: 'Inner wall 3', type: 'wall', length: 1500, angle: 90 },
 
-    // Inner walls Toilet
+    // Inner walls toilet
     { start: 'A4', end: 'A8', name: 'Inner wall 4', type: 'wall', length: 1500, angle: 270 },
-    { start: 'A8', end: 'A9', name: 'Inner wall 5', type: 'wall', length: 1800, angle: 0 },
+    { start: 'A8', end: 'A9', name: 'Inner wall 5', type: 'wall', length: 1200, angle: 0 },
     { start: 'A9', end: 'A10', name: 'Inner wall 6', type: 'wall', length: 1500, angle: 90 },
 
-    // Pathway
-    { start: 'A1', end: 'A11', name: 's wall 1', type: 'wall', length: 2000, angle: 90 },
-    { start: 'A11', end: 'A12', name: 'Pathway wall 2', type: 'wall', length: 1000, angle: 0 },
-    { start: 'A12', end: 'A13', name: 'Pathway wall 3', type: 'wall', length: 2000, angle: 270 },
+    // Doors
+    { start: 'A2', end: 'A11', name: 'Door 1', type: 'door', length: 1500, angle: 180 },
+    { start: 'A5', end: 'A12', name: 'Door 2', type: 'door', length: 900, angle: 180 },
+    { start: 'A9', end: 'A13', name: 'Door 3', type: 'door', length: 900, angle: 90 },
 
-    // Room door
-    { start: 'A2', end: 'A14', name: 'Door 1', type: 'door', length: 1500, angle: 180 },
+    // Windows
+    { start: 'A1', end: 'A14', name: 'Window External wall 1', type: 'wall', length: 500, angle: 0 },
+    { start: 'A14', end: 'A15', name: 'Window 1', type: 'window', length: 1000, angle: 0 },
 
-    // Kitchen door
-    { start: 'A5', end: 'A15', name: 'Door 2', type: 'door', length: 900, angle: 180 },
+    { start: 'A3', end: 'A16', name: 'Window External wall 2', type: 'wall', length: 500, angle: 180 },
+    { start: 'A16', end: 'A17', name: 'Window 2', type: 'window', length: 1000, angle: 180 },
 
-    // Toilet door
-    { start: 'A9', end: 'A16', name: 'Door 3', type: 'door', length: 900, angle: 90 },
+    { start: 'A4', end: 'A18', name: 'Window External wall 3', type: 'wall', length: 500, angle: 0 },
+    { start: 'A18', end: 'A19', name: 'Window 3', type: 'window', length: 1000, angle: 0 },
+  ];
 
-    // Room Window
-    { start: 'A1', end: 'A17', name: 'Window External wall 1', type: 'wall', length: 500, angle: 0 },
-    { start: 'A17', end: 'A18', name: 'Window 1', type: 'window', length: 1000, angle: 0 },
 
-    // Kitchen Window
-    { start: 'A3', end: 'A19', name: 'Window External wall 2', type: 'wall', length: 250, angle: 180 },
-    { start: 'A19', end: 'A20', name: 'Window 2', type: 'window', length: 800, angle: 180 },
-
-    // Toilet Window
-    { start: 'A4', end: 'A21', name: 'Window External wall 3', type: 'wall', length: 250, angle: 0 },
-    { start: 'A21', end: 'A22', name: 'Window 3', type: 'window', length: 500, angle: 0 },
-];
 
 
 
@@ -392,9 +383,11 @@ setupEventListeners() {
     this.drawingService.deleteSelectedShapes();
   }
 
-  async drawSquare() {
+  async drawSquare(segment : any) {
     // Draw a square starting at (100, 100) with side length 200
-    await this.autoDrawService.drawShape(this.segments1); // Start drawing at (100, 100)
+    this.drawingService.clearAllDrawings();
+    await this.autoDrawService.drawShape(segment); // Start drawing at (100, 100)
+
     this.currentScale = this.autoDrawService.checkShapeSize();
 
     this.setMode('select')
