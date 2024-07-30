74f476b7ee87793e7addec8175d78a7658e54e02
lordbsqaure <tse2ndi@gmail.com>
2024-07-30 11:38:14 +0100

    design(status bar):added status bar to application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.initializeApp();
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.setStatusBarColor();
+    });
+  }
+  async setStatusBarColor() {
+    // Set the status bar color to match your header
+    await StatusBar.setBackgroundColor({ color: '#fefefe' }); // Use your header color here
+
+    // Optionally set the style to default, dark, or light
+    await StatusBar.setStyle({ style: Style.Default });
+  }
 }
